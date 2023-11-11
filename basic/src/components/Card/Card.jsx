778eae03aa722d20a5c785c1178fc55c94f43ed8
ys <241280843@qq.com>
2023-11-12 01:42:05 +0800

    HOC Demo v1

--- basic/src/components/Card/Card.jsx
+++ basic/src/components/Card/Card.jsx
@@ -1,18 +1,25 @@
-/* eslint-disable react-refresh/only-export-components */
-import HOC from '../HOC/HOC';
-
 /* eslint-disable react/prop-types */
-function Card({ ...props }) {
+/* eslint-disable react-refresh/only-export-components */
+import { useEffect } from 'react';
+import hoc from '../HOC/HOC';
+
+
+ function Card(props) {
+  const {onLoading} = props;
+
+  useEffect(()=>{
+    if(onLoading){
+      onLoading((loading)=>{
+        console.log('正在加载',loading);
+      })
+    }
+    console.log("card");
+  },[onLoading])
+
   return (
-    <div>
-      Card
+    <div style={{width:200,height:200,backgroundColor:'#fff'}} {...props}>
       {props.children}
     </div>
-  );
+  )
 }
-
-export default HOC(Card, function asyncFunc() {
-  setTimeout(() => {
-    console.log('success');
-  }, 2000);
-});
+export default hoc(Card,2000,"Card Title");
--- basic/src/components/HOC/HOC.jsx
+++ basic/src/components/HOC/HOC.jsx
@@ -6,19 +6,33 @@ import { useEffect, useState } from 'react';
   定义：接收一个组件参数，返回一个新的组件
   用途：用于封装一些通用逻辑以增强传入组件的功能，添加一些通用样式等（提高复用性）
 */
-export default function HOC({ InnerComponent, asycnFunc, ...props }) {
-  return (props) => {
-    const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-      async function getData() {
-        setIsLoading(true);
-        await asycnFunc();
-        setIsLoading(false);
+/*
+  拥有红色Title，以及loading状态, 并且传递onLoading事件
+*/
+export default function hoc(InnerComponent,delay,title) {
+  return function(props) {
+    const [loading,setLoading] = useState(false);
+
+    useEffect(()=>{
+      async function asyncFunc(){
+        setLoading(true);
+        setTimeout(()=>{
+          setLoading(false);
+        },delay);
       }
-      getData();
-    }, []);
-
-    return <InnerComponent {...props}>{isLoading ? 'loading...' : undefined}</InnerComponent>;
-  };
+      asyncFunc();
+      console.log("hoc");
+    },[])
+    return <>
+      {loading ? 
+      <InnerComponent {...props} onLoading={(func)=>{ return func(loading)}}>
+        <span style={{color:'black'}}>Loading...</span>
+      </InnerComponent> : 
+      <InnerComponent onLoading={(func)=>{ return func(loading)}} {...props}>
+        <span style={{color:'red'}}>{title}</span>
+      </InnerComponent>}
+    </>
+  }
 }
+
