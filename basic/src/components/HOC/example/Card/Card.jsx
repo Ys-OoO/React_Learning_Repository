1025ee602c7b13181a18eea7c8ad1cf63dec1aed
ys <241280843@qq.com>
2023-11-12 16:24:06 +0800

    HOC

---
+++ basic/src/components/HOC/example/Card/Card.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react-refresh/only-export-components */
+import { useEffect } from 'react';
+import hoc from '../HOC/HOC';
+
+
+ function Card(props) {
+  const {onLoading,title} = props;
+
+  useEffect(()=>{
+    if(onLoading){
+      onLoading((loading)=>{
+        console.log('正在加载',loading);
+      })
+    }
+    console.log("card");
+  },[onLoading])
+
+  return (
+    <div style={{width:200,height:200,backgroundColor:'#fff'}} {...props}>
+      {title}
+    </div>
+  )
+}
+export default hoc(Card,2000,"Card Title");
