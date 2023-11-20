72d5e7c0b8e9a4fa1d381b98d0db2d7669bf080a
ys <yangsenwb303@hellobike.com>
2023-11-20 12:31:34 +0800

    截屏换肤

--- basic/src/App.jsx
+++ basic/src/App.jsx
@@ -1,22 +1,34 @@
-import Demo from './components/Theme/CssVar/Demo';
-
+import { useContext, useRef, useState } from 'react';
+import ThemeContext from './components/Theme/ThemeContext/ThemeContext';
+import { crop, toCanvas } from './utils/canvasUtils';
 function App() {
+  const themeContext = useContext(ThemeContext);
+  const [theme, setTheme] = useState(themeContext);
+  const themeStyle =
+    theme === 'light'
+      ? { backgroundColor: '#fff', color: '#333' }
+      : { backgroundColor: '#333', color: '#fff' };
+  const parentRef = useRef();
+  const targetRef = useRef();
+
+  const toggleTheme = (e) => {
+    toCanvas(targetRef.current).then((canvas) => {
+      parentRef.current.appendChild(canvas);
+      crop(canvas, e, { reverse: theme === 'dark' }).then((canvas) => {
+        parentRef.current.removeChild(canvas);
+      });
+      setTheme(theme === 'light' ? 'dark' : 'light');
+    });
+  };
+
   return (
-    <>
-      <Demo />
-      <button
-        onClick={() => {
-          const classList = document.documentElement.classList;
-          if (classList.value) {
-            document.documentElement.classList.remove('dark');
-            return;
-          }
-          document.documentElement.classList.add('dark');
-        }}
-      >
-        主题切换
-      </button>
-    </>
+    <ThemeContext.Provider value={theme}>
+      <div ref={parentRef}>
+        <div style={{ width: 200, height: 400, ...themeStyle }} ref={targetRef}>
+          <button onClick={toggleTheme}>👻点赞+收藏👻</button>
+        </div>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
